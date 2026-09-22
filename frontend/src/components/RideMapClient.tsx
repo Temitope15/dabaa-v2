18e670b8eb8f3697aa8d0e0e6de9fb0608d49128
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2026-09-22 12:02:14 +0100

    Fix map watermark by switching to standard OSM tiles

--- frontend/src/components/RideMapClient.tsx
+++ frontend/src/components/RideMapClient.tsx
@@ -115,7 +115,7 @@ export default function RideMapClient({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         
         {/* User Location */}
